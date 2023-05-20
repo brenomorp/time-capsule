3f1bf7942441788c94057b84a30853acd9860808
brenomorp <brenomoreirapro@gmail.com>
2023-05-20 16:04:22 -0300

    create components and fix prettier conflict with eslint

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,17 @@
+function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        href="https://www.rocketseat.com.br"
+        className="underline transition-colors hover:text-gray-50"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
+
+export default Copyright
