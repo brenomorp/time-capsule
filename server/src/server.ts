3e268f10748ac3d411aa9599f26ae581d8d26d08
brenomorp <brenomoreirapro@gmail.com>
2023-05-19 22:05:05 -0300

    CRUD memories

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,18 @@
-import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import fastify from "fastify";
+import { memoriesRoutes } from "./routes/memories";
+import cors from "@fastify/cors";
 
-const app = fastify()
-const prisma = new PrismaClient()
+const app = fastify();
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
-})
+app.register(cors, {
+  origin: true,
+});
+app.register(memoriesRoutes);
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('deu bom')
-  })
+    console.log("deu bom");
+  });
